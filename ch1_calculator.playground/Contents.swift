2ddeaaa0a28346ef2811c859f92db3ff2dd1f974
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-03-06 11:33:15 +0900

    lv.1 clear

---
+++ ch1_calculator.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class Calculator {
+    // Todo : 내부 구현하기
+    func calculate(operation: String, firstNumber: Double, secondNumber: Double) -> Double {
+        switch operation {
+        case "+":
+            return (firstNumber + secondNumber)
+        case "-":
+            return (firstNumber - secondNumber)
+        case "/":
+            return firstNumber / secondNumber
+        case "*":
+            return (firstNumber * secondNumber)
+        default:
+            return -1
+        }
+    }
+}
+
+let calculator = Calculator() // 인스턴스 생성하여 변수에 할당
+
+// Todo : calculator 변수를 활용하여 사칙연산을 진행
+let addResult = calculator.calculate(operation: "+", firstNumber: 10, secondNumber: 20)
+let subtractResult = calculator.calculate(operation: "-", firstNumber: 10, secondNumber: 20)
+let multiplyResult = calculator.calculate(operation: "*", firstNumber: 10, secondNumber: 20)
+let divideResult = calculator.calculate(operation: "/", firstNumber: 10, secondNumber: 20)
+
+print("addResult : \(addResult)")
+print("subtractResult : \(subtractResult)")
+print("multiplyResult : \(multiplyResult)")
+print("divideResult : \(divideResult)")
